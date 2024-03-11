83bdbacec6e5ed0fa9d5fdacd9b8e63c4625e20e
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-03-11 17:06:04 +0000

    airplane route urls

---
+++ airplane/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import AirplaneViewSet
+
+router = routers.DefaultRouter()
+router.register(r'airplanes', AirplaneViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+urlpatterns += router.urls
