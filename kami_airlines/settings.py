c473b5c3c3f450c11556eedad75926685595fe0b
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-03-11 17:03:50 +0000

    init airplane app

--- kami_airlines/settings.py
+++ kami_airlines/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'airplane'
 ]
 
 MIDDLEWARE = [
@@ -98,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
